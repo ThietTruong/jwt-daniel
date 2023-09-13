5f29b5baeb2e19c17359239967e98071a87761b4
Thiet Truong <thiettruong1999@gmail.com>
2023-09-13 11:42:15 +0700

    clean login and register function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -27,25 +27,27 @@ const authController = {
     }
   },
 
-  loginUser: async (req, res) => {
-    try {
-      const user = await User.findOne({ email: req.body.email });
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-      const passwordCorrect = await bcrypt.compare(
-        req.body.password,
-        user.password
-      );
-      if (!passwordCorrect) {
-        return res.status(400).json({ error: "Invalid password" });
-      }
-      return res.status(200).json(user);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "Login failed" });
+loginUser: async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
+    
+    const passwordCorrect = await bcrypt.compare(password, user.password);
+    
+    if (!passwordCorrect) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+    
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Login failed" });
   }
+}
 };
 
 module.exports = authController;
